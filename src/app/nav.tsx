b635d77251c24d0116d03a48c38c1466bff03414
Michelle Santiago <mdcsantiago0@gmail.com>
2023-09-04 09:03:05 +0800

    fix: add missing pointer events

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -8,7 +8,7 @@ export default function Nav(){
   return (
     <>
       <div className="flex flex-1 justify-end md:justify-center">
-        <nav className="hidden md:block">
+        <nav className="pointer-events-auto hidden md:block">
           <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
             <li>
               <Link href="/" className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-yellow-500">
@@ -34,7 +34,7 @@ export default function Nav(){
         </nav>
       </div>
 
-      <div className="md:hidden">
+      <div className="pointer-events-auto md:hidden">
         <Sheet>
           <SheetTrigger><TbCircleLetterM size={40}/></SheetTrigger>
           <SheetContent>
